0526e5f83a5acc6802a6ca25446985a2f710c798
codezera <codezera3@gmail.com>
2024-01-08 22:57:17 +0530

    auth service setup and bcrypt setup

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,29 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { AuthDto } from './dto/auth.dto';
+import { Tokens } from './types';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('/signup')
+  signup(@Body() authDto: AuthDto): Promise<Tokens> {
+    return this.authService.signup(authDto);
+  }
+
+  @Post('/signin')
+  signin() {
+    return this.authService.signup();
+  }
+
+  @Post('/logout')
+  logout() {
+    return this.authService.signup();
+  }
+
+  @Post('/refresh')
+  refreshTokens() {
+    return this.authService.signup();
+  }
+}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,29 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto/auth.dto';
+import * as bcrypt from 'bcrypt';
+import { Tokens } from './types';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  hashData(data: string) {
+    return bcrypt.hash(data, 10);
+  }
+  async signup(authDto: AuthDto): Promise<Tokens> {
+    const hashedPassword = await this.hashData(authDto.password);
+    const newUser = await this.prisma.user.create({
+      data: {
+        email: authDto.email,
+        hash: hashedPassword,
+      },
+    });
+  }
+
+  signin() {}
+
+  logout() {}
+
+  refreshTokens() {}
+}
