612d3317fd4f85910222059a552c8e922de2739b
codezera <codezera3@gmail.com>
2024-01-10 23:29:45 +0530

    added and implemented jwt strategies

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,29 +1,48 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  HttpCode,
+  HttpStatus,
+  Param,
+  ParseIntPipe,
+  Post,
+  Req,
+  UseGuards,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto/auth.dto';
 import { Tokens } from './types';
+import { AuthGuard } from '@nestjs/passport';
+import { Request } from 'express';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  @Post('/signup')
+  @Post('signup')
+  @HttpCode(HttpStatus.CREATED)
   signup(@Body() authDto: AuthDto): Promise<Tokens> {
     return this.authService.signup(authDto);
   }
 
-  @Post('/signin')
-  signin() {
-    return this.authService.signup();
+  @Post('signin')
+  @HttpCode(HttpStatus.OK)
+  signin(@Body() autDto: AuthDto): Promise<Tokens> {
+    return this.authService.signin(autDto);
   }
 
-  @Post('/logout')
-  logout() {
-    return this.authService.signup();
+  @UseGuards(AuthGuard('jwt'))
+  @Post('logout')
+  @HttpCode(HttpStatus.OK)
+  logout(@Req() req: Request) {
+    const user = req.user;
+    return this.authService.logout(user['id']);
   }
 
+  @UseGuards(AuthGuard('jwt-refresh'))
   @Post('/refresh')
+  @HttpCode(HttpStatus.OK)
   refreshTokens() {
-    return this.authService.signup();
+    return this.authService.refreshTokens();
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,64 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthDto } from './dto/auth.dto';
 import * as bcrypt from 'bcrypt';
 import { Tokens } from './types';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly jwtService: JwtService,
+  ) {}
 
   hashData(data: string) {
     return bcrypt.hash(data, 10);
   }
+
+  async generateTokens(userId: number, email: string) {
+    const [at, rt] = await Promise.all([
+      this.jwtService.signAsync(
+        {
+          sub: userId,
+          email,
+        },
+        {
+          secret: 'at-secret',
+          expiresIn: 60 * 15,
+        },
+      ),
+      this.jwtService.signAsync(
+        {
+          sub: userId,
+          email,
+        },
+        {
+          secret: 'rt-secret',
+          expiresIn: 60 * 60 * 60 * 24 * 7,
+        },
+      ),
+    ]);
+
+    return {
+      access_token: at,
+      refresh_token: rt,
+    };
+  }
+
+  async updateRtHash(userId: number, rt: string) {
+    const hash = await this.hashData(rt);
+
+    await this.prisma.user.update({
+      where: {
+        id: userId,
+      },
+      data: {
+        hasedRt: hash,
+      },
+    });
+  }
+
   async signup(authDto: AuthDto): Promise<Tokens> {
     const hashedPassword = await this.hashData(authDto.password);
     const newUser = await this.prisma.user.create({
@@ -19,11 +67,44 @@ export class AuthService {
         hash: hashedPassword,
       },
     });
+
+    const tokens = await this.generateTokens(newUser.id, newUser.email);
+    await this.updateRtHash(newUser.id, tokens.refresh_token);
+    return tokens;
   }
 
-  signin() {}
+  async signin(authDto: AuthDto) {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: authDto.email,
+      },
+    });
 
-  logout() {}
+    if (!user) {
+      throw new ForbiddenException('No such user found!');
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(authDto.password, user.hash);
+
+    if (!isPasswordCorrect) throw new ForbiddenException('Incorrect Password');
+
+    const tokens = await this.generateTokens(user.id, user.email);
+
+    await this.updateRtHash(user.id, tokens.refresh_token);
+
+    return tokens;
+  }
+
+  async logout(id: number) {
+    await this.prisma.user.update({
+      where: {
+        id,
+      },
+      data: {
+        hasedRt: null,
+      },
+    });
+  }
 
   refreshTokens() {}
 }
